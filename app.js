d3346b518ab2cc2a6841cf8c6bd09bd4e8a99f9d
francis8t <francis8t@gmail.com>
2024-11-14 16:39:13 +0300

    testing #6

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, OVHCloud!');
+    res.send('Hello, OVHCloud! This App has been deployed through GitHub Actions automatically!');
 });
 
 app.listen(PORT, () => {
